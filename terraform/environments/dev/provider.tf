5f5923729cf9b722ac13abd8a3e9ed0cb0a9ef5d
B4xim <bk6549@srmist.edu.in>
2026-09-19 14:33:20 +0530

    Added Subnets

--- terraform/environments/dev/provider.tf
+++ terraform/environments/dev/provider.tf
@@ -17,8 +17,46 @@ resource "aws_vpc" "main" {
     Environment = local.environment
   }
 }
+resource "aws_subnet" "public_1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.public_subnet_cidr_1
+  availability_zone = var.availability_zone_1
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "${local.project_name}-public-1"
+    Environment = local.environment
+  }
+}
+resource "aws_subnet" "public_2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.public_subnet_cidr_2
+  availability_zone = var.availability_zone_2
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "${local.project_name}-public-2"
+    Environment = local.environment
+  }
+}
+resource "aws_subnet" "private_1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_cidr_1
+  availability_zone = var.availability_zone_1
+  tags = {
+    Name = "${local.project_name}-private-1"
+    Environment = local.environment
+  }
+}
+resource "aws_subnet" "private_2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_cidr_2
+  availability_zone = var.availability_zone_2 
+  tags = {
+    Name = "${local.project_name}-private-2"
+    Environment = local.environment
+  }
+}
 
 locals {
-  project_name = "cloudnative-eks-platform"
+  project_name = "cloudnative-eks-dev-platform"
   environment  = "dev"
 }
--- terraform/environments/dev/variables.tf
+++ terraform/environments/dev/variables.tf
@@ -2,3 +2,28 @@ variable "vpc_cidr" {
   default = "10.0.0.0/16"
   description = "The CIDR block for the VPC."
 }
+variable "public_subnet_cidr_1" {
+  default = "10.0.1.0/24"
+  description = "The CIDR block for the first public subnet."
+  }
+
+variable "public_subnet_cidr_2" {
+  default = "10.0.2.0/24"
+  description = "The CIDR block for the second public subnet."
+}
+variable "private_subnet_cidr_1" {
+  default = "10.0.3.0/24"
+  description = "The CIDR block for the first private subnet."
+}
+variable "private_subnet_cidr_2" {
+  default = "10.0.4.0/24"
+  description = "The CIDR block for the second private subnet."
+}
+variable "availability_zone_1" {
+  default = "us-east-1a"
+  description = "The first availability zone."
+}
+variable "availability_zone_2" {
+  default = "us-east-1b"
+  description = "The second availability zone."
+}
