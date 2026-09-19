8e43dcb1c3de6969feed2f1dd12db88ca6305c27
B4xim <bk6549@srmist.edu.in>
2026-09-19 14:38:54 +0530

    Aws route table association added

--- terraform/environments/dev/provider.tf
+++ terraform/environments/dev/provider.tf
@@ -17,6 +17,20 @@ resource "aws_vpc" "main" {
     Environment = local.environment
   }
 }
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "${local.project_name}-igw"
+    Environment = local.environment
+  }
+}
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+}
 resource "aws_subnet" "public_1" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.public_subnet_cidr_1
@@ -27,6 +41,7 @@ resource "aws_subnet" "public_1" {
     Environment = local.environment
   }
 }
+
 resource "aws_subnet" "public_2" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.public_subnet_cidr_2
@@ -54,6 +69,22 @@ resource "aws_subnet" "private_2" {
     Name = "${local.project_name}-private-2"
     Environment = local.environment
   }
+}
+resource "aws_route_table_association" "public_1" {
+  subnet_id      = aws_subnet.public_1.id
+  route_table_id = aws_route_table.public.id
+}
+resource "aws_route_table_association" "public_2" {
+  subnet_id      = aws_subnet.public_2.id
+  route_table_id = aws_route_table.public.id
+}
+resource "aws_route_table_association" "private_1" {
+  subnet_id      = aws_subnet.private_1.id
+  route_table_id = aws_route_table.public.id
+}
+resource "aws_route_table_association" "private_2" {
+  subnet_id      = aws_subnet.private_2.id
+  route_table_id = aws_route_table.public.id
 }
 
 locals {
